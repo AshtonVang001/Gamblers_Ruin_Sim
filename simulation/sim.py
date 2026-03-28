0f8d4cb4c61ff86eab0fe769936aa06e74040fe5
Ashton Vang <ashtonvang100@gmail.com>
2026-03-28 13:46:22 -0700

    feat: add conditions to player feature

--- simulation/sim.py
+++ simulation/sim.py
@@ -2,27 +2,36 @@ from rules import Rules
 import random
 
 class Simulation(Rules):
-    def __init__(self, bet=1):
+    def __init__(self, bet=1, rounds=0):
         super().__init__()
         self.bet = bet
+        self.rounds = rounds
     
 
-    #here user will play
-    #have a way to where the user decides when they want to bet (like a buttom or input)
     def simBets(self):
         if random.random() < self.probability:
             self.money += self.bet
         else:
             self.money -= self.bet
 
-    
+    #right now player money is not saving
+    #need state for the money
+    #also right now game is stopping after each bet - which should not happen
+    #game should only stop when player wants it to, they run out of money, or they reach the goal
     def play(self):
-        willPlay = input("Would you like to bet y/n?")
-        if willPlay == "y":
-            self.simBets()
-            print("You have $", self.money, "left")
-        else:
-            print("Ok")
+        willPlay = input("Would you like to play y/n?")
+        if(willPlay == "y"):
+            while(self.money != self.goal and self.money != self.lose_condition): #this is the wrong loop
+                willBet = input("Would you like to bet y/n?")
+                if willBet == "y":
+                    self.rounds += 1
+                    self.simBets()
+                    print("You have $", self.money, "left")
+                else:
+                    print("Ok")
+                    break;
+        print("Money left: ", self.money)
+        print("Rounds played: ", self.rounds)
 
 player = Simulation();
 player.play();
