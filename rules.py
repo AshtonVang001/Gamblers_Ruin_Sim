82378184c963b0c7a977cf3f5f624396fe870e3a
Ashton Vang <ashtonvang100@gmail.com>
2026-03-28 12:27:52 -0700

    add: add rules class

---
+++ rules.py
@@ -0,0 +1,7 @@
+#rules class
+class Rules:
+    def __init__(self, money=10, goal=20, lose_condition=0, probability=0.5):
+        self.money = money
+        self.goal = goal
+        self.lose_condition = lose_condition
+        self.probability = probability
